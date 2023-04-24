414dd96a8bef527b8a6fce491980a15f603e670b
PK Iyer <92050296+piyerf5@users.noreply.github.com>
2023-04-24 14:48:19 -0400

    Delete output.tf

--- output.tf
+++
@@ -1,9 +0,0 @@
-output k8s_cluster_name {
-    value = volterra_k8s_cluster.example.name
-    description = "This is the name of the K8s cluster using the Voltstack Combo image"
-}
-
-output k8s_cluster_namespace {
-    value = volterra_k8s_cluster.example.namespace
-    description = "This is an F5 Distributed Cloud namespace, not a K8s cluster namespace"
-}
